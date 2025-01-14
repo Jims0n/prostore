125b43b7812a8c2307c5e1c250ac4ad47b29b78c
Jimson <opeyemiabdulateef406@gmail.com>
2025-01-14 15:11:22 +0100

    payment-method & place-order page created, order schemas, order action

--- types/index.ts
+++ types/index.ts
@@ -1,13 +1,24 @@
 import { z } from "zod";
-import { cartItemSchema, insertCartSchema, insertProductSchema, shippingAddressSchema } from "@/lib/validator";
+import { cartItemSchema, insertCartSchema, insertProductSchema, shippingAddressSchema, insertOrderSchema, insertOrderItemSchema, } from "@/lib/validator";
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
   createdAt: Date;
   rating: string;
   numReviews: number;
-}
+};
 
-export type Cart = z.infer<typeof insertCartSchema>
-export type CartItem = z.infer<typeof cartItemSchema>
-export type ShippingAddress = z.infer<typeof shippingAddressSchema>
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: { name: string; email: string };
+};
