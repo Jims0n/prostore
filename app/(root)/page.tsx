d359ff215f45cb5e7d951403c365737374789ceb
Jimson <opeyemiabdulateef406@gmail.com>
2025-03-20 15:39:52 +0100

    add category drawer and featured products carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.action";
+import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.action";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
-  
+  const featuredProducts = await getFeaturedProducts();
   return <>
+    { featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
     <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
   </>;
 }
