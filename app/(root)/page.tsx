abbcc4064e2f2a62e537895b3b1ce2d8c1849e66
Jimson <opeyemiabdulateef406@gmail.com>
2025-03-26 17:08:45 +0100

    add icon boxes and deal count-down component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,8 @@ import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.action";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
+import DealCountdown from "@/components/deal-countdown";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
@@ -10,6 +12,8 @@ const Homepage = async () => {
     { featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
     <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     <ViewAllProductsButton />
+    <DealCountdown />
+    <IconBoxes />
   </>;
 }
  
